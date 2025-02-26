fe56786b1e8669a4e1681de19e4334f7d56f6608
Leonardo782-crypto <leonardoalvesgomez1@gmail.com>
2025-02-26 19:41:26 -0300

    coloquei print "oi" e criei um novo programa que pergunta o nome e responde

---
+++ aulas.py
@@ -0,0 +1,2 @@
+nome=input('Qual é o seu nome?')
+print('prazer em conhecelo {:_^20}'.format(nome))
--- ex003.py
+++ ex003.py
@@ -2,3 +2,4 @@ n1 = int(input("digite um valor :"))
 n2 = int(input("digite outro valor :"))
 s = n1 + n2
 print(" a soma entre {} e {} é igual a {}!".format(n1,n2,s))
+print("Oi")
