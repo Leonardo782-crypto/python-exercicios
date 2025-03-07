0bdfdb2d7c6d8cbcc2eab2dd6a4be54d3eef2d12
Leonardo782-crypto <leonardoalvesgomez1@gmail.com>
2025-03-06 22:02:03 -0300

    fiz mais exercícios de python,aprendi o comando import e diz tudo de cabeça

---
+++ ex018.py
@@ -0,0 +1,3 @@
+import random
+num=random.randint(1,10)
+print('Um número gerado aleatoriamente entre 1 e 10 é {}'.format(num))
---
+++ ex020.py
@@ -0,0 +1,4 @@
+num=float(input('Digite um valor: '))
+import math
+print('o número em porção inteira é {}'.format(math.trunc(num)))
+
